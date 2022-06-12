0d30d90c7c44576585e1a36d4c61368934369de6
Daniel Byrne <dbyrne978@gmail.com>
2022-06-11 20:43:33 -0400

    Refactor App component into multiple components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Backlog Tracker</h1>
-      <p>Media 1</p>
-      <p>Media 2</p>
-      <p>Media 3</p>
-      <footer>
-        Webpage by: 
-        <a href="https://dbyrne978.github.io/portfolio/">Daniel Byrne</a>
-      </footer>
+      <Header />
+      <MediaCard number="1" />
+      <MediaCard number="2" />
+      <MediaCard number="3" />
+      <Footer />
     </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <>
+      <h1>Backlog Tracker</h1>
+    </>
+  );
+}
+
+const MediaCard = (props) => {
+  return (
+    <>
+      <p>dummy media card {props.number}</p>
+    </>
+  );
+}
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        Webpage by:
+        <a href="https://dbyrne978.github.io/portfolio/"> Daniel Byrne</a>
+      </footer>
+    </>
   );
 }
 
