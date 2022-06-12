ab1e0cee02bb20537826d2f76f3f9d11cce92c57
Daniel Byrne <dbyrne978@gmail.com>
2022-06-11 21:57:32 -0400

    Put all constants into one object

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 const App = () => {
-  const userName = "Dan"
-  const defaultCards = [
-    {
-      title: "13 Sentinels",
-      mediaType: "Video Game"
-    },
-    {
-      title: "Buffy: the Vampire Slayer",
-      mediaType: "TV Show"
-    },
-    {
-      title: "Chronicle",
-      mediaType: "Movie"
-    },
-    {
-      title: "Blood of Elves",
-      mediaType: "Book"
-    }
-  ]
+  const backLog = {
+    userName: "Dan",
+    defaultCards: [
+      {
+        title: "13 Sentinels",
+        mediaType: "Video Game"
+      },
+      {
+        title: "Buffy: the Vampire Slayer",
+        mediaType: "TV Show"
+      },
+      {
+        title: "Chronicle",
+        mediaType: "Movie"
+      },
+      {
+        title: "Blood of Elves",
+        mediaType: "Book"
+      }
+    ]
+  }
 
   return (
     <div className="App">
-      <Header userName={userName} />
-      <MediaCard {...defaultCards[0]} />
-      <MediaCard {...defaultCards[1]} />
-      <MediaCard {...defaultCards[2]} />
-      <MediaCard {...defaultCards[3]} />
+      <Header userName={backLog.userName} />
+      <MediaCard {...backLog.defaultCards[0]} />
+      <MediaCard {...backLog.defaultCards[1]} />
+      <MediaCard {...backLog.defaultCards[2]} />
+      <MediaCard {...backLog.defaultCards[3]} />
       <br /><br />
       <Footer />
     </div>
